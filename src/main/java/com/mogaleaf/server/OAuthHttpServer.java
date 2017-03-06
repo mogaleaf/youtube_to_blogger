cfe7015ac0a7ecd23f165420f964fc87a7896b04
Cecile <cecile.gomes@mogaleaf.com>
2017-03-06 11:18:48 -0500

    retrieve port by env

--- src/main/java/com/mogaleaf/server/OAuthHttpServer.java
+++ src/main/java/com/mogaleaf/server/OAuthHttpServer.java
@@ -8,7 +8,8 @@ import java.net.InetSocketAddress;
 public class OAuthHttpServer {
 
     public void start() throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8888), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(Integer.valueOf(System.getenv("PORT"))), 0);
+      //  HttpServer server = HttpServer.create();
 
         server.createContext("/connect", new OauthHttpHandler());
         server.createContext("/work", new ConnectedHandler());
