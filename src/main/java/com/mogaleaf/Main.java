7e6d583ef044a4d12f540a294c0d879e2ff21a63
Cecile <cecile.gomes@mogaleaf.com>
2017-03-06 12:22:06 -0500

    add log

--- src/main/java/com/mogaleaf/Main.java
+++ src/main/java/com/mogaleaf/Main.java
@@ -5,17 +5,21 @@ import com.mogaleaf.server.Configuration;
 import com.mogaleaf.server.OAuthHttpServer;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
 
     public static void main(String args[]) {
+        Logger.getLogger("APPBLOG").info("STARTING");
         try {
             File f = new File("token");
             Configuration.DATA_STORE_FACTORY = new FileDataStoreFactory(f);
             new OAuthHttpServer().start();
 
         } catch (Exception e) {
+            Logger.getLogger("APPBLOG").log(Level.SEVERE,"problem",e);
             e.printStackTrace();
         }
     }
--- src/main/java/com/mogaleaf/server/OAuthHttpServer.java
+++ src/main/java/com/mogaleaf/server/OAuthHttpServer.java
@@ -4,16 +4,20 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.logging.Logger;
 
 public class OAuthHttpServer {
 
     public void start() throws IOException {
+
         HttpServer server = HttpServer.create(new InetSocketAddress(Integer.valueOf(System.getenv("PORT"))), 0);
-      //  HttpServer server = HttpServer.create();
+        Logger.getLogger("APPBLOG").info("SERVER CREATED");
 
         server.createContext("/connect", new OauthHttpHandler());
         server.createContext("/work", new ConnectedHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
+        Logger.getLogger("APPBLOG").info("SERVER STARTED");
+        Logger.getLogger("APPBLOG").info(server.getAddress().getHostString());
     }
 }
