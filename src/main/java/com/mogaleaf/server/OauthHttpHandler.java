4d5f2e506319233b6482030d85836bdd3f613ca6
Cecile <cecile.gomes@mogaleaf.com>
2017-04-25 12:28:11 -0400

    more logs

--- src/main/java/com/mogaleaf/server/OauthHttpHandler.java
+++ src/main/java/com/mogaleaf/server/OauthHttpHandler.java
@@ -11,35 +11,50 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class OauthHttpHandler implements HttpHandler {
 
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        Map<String, String> queryMap = HttpUtils.queryToMap(httpExchange.getRequestURI().getQuery());
-        String id = queryMap.get(Configuration.ID_PARAM);
-        if (id == null) {
-            id = Configuration.ID;
-        }
-        String code = queryMap.get(Configuration.CODE_PARAM);
-        GoogleAuthorizationCodeFlow flow = OAuthUtils.retrieveFlow();
-        Credential credential = flow.loadCredential(id);
-        if (credential == null && code == null) {
-            requestCredentials(httpExchange, id, flow);
-        } else {
-            if (code != null) {
-                requestCode(id, code, flow);
+        try {
+            Logger.getLogger("APPBLOG").info("connect servlet");
+            Map<String, String> queryMap = HttpUtils.queryToMap(httpExchange.getRequestURI().getQuery());
+            String id = queryMap.get(Configuration.ID_PARAM);
+            if (id == null) {
+                id = Configuration.ID;
             }
-            HttpUtils.redirectTo(Configuration.CONNECTED_HANDLER + id, httpExchange);
+            Logger.getLogger("APPBLOG").info("id " + id);
+            String code = queryMap.get(Configuration.CODE_PARAM);
+            Logger.getLogger("APPBLOG").info("code " + code);
+            GoogleAuthorizationCodeFlow flow = OAuthUtils.retrieveFlow();
+            Credential credential = flow.loadCredential(id);
+            if (credential == null && code == null) {
+                Logger.getLogger("APPBLOG").info("requestCredentials ");
+                requestCredentials(httpExchange, id, flow);
+            } else {
+                if (code != null) {
+                    Logger.getLogger("APPBLOG").info("requestcode ");
+                    requestCode(id, code, flow);
+                }
+                HttpUtils.redirectTo(Configuration.CONNECTED_HANDLER + id, httpExchange);
+            }
+        }catch(Exception e){
+            Logger.getLogger("APPBLOG").log(Level.SEVERE,"prob",e);
         }
     }
 
 
     private void requestCode(String id, String code, GoogleAuthorizationCodeFlow flow) throws IOException {
+        Logger.getLogger("APPBLOG").info("newTokenRequest " );
         GoogleAuthorizationCodeTokenRequest googleAuthorizationCodeTokenRequest = flow.newTokenRequest(code);
+        Logger.getLogger("APPBLOG").info("setRedirectUri " );
         googleAuthorizationCodeTokenRequest.setRedirectUri(Configuration.CONNECT_URI + id);
+        Logger.getLogger("APPBLOG").info("execute " );
         GoogleTokenResponse execute = googleAuthorizationCodeTokenRequest.execute();
+        Logger.getLogger("APPBLOG").info("store " );
         flow.createAndStoreCredential(execute, id);
 
     }
