4305889768043e097aaa1617f36ea3c1785ee74e
cecile <cecile@nulab-inc.com>
2017-10-11 09:24:53 -0400

    Use pool when create datastore

--- src/main/java/com/mogaleaf/oauth/RedisDatastoreFactory.java
+++ src/main/java/com/mogaleaf/oauth/RedisDatastoreFactory.java
@@ -4,20 +4,21 @@ import com.google.api.client.auth.oauth2.StoredCredential;
 import com.google.api.client.util.store.AbstractDataStoreFactory;
 import com.google.api.client.util.store.DataStore;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.io.IOException;
 
 public class RedisDatastoreFactory extends AbstractDataStoreFactory
 {
-	private Jedis jedis;
-	public RedisDatastoreFactory(Jedis jedis){
-		this.jedis = jedis;
+	private JedisPool jedisPool;
+	public RedisDatastoreFactory(JedisPool jedis){
+		this.jedisPool = jedis;
 	}
 
 
 	@Override
 	protected DataStore<StoredCredential> createDataStore(String s) throws IOException {
-			DataStore<StoredCredential> redisDataStore = new RedisDataStore(this,s,jedis);
+			DataStore<StoredCredential> redisDataStore = new RedisDataStore(this,s,jedisPool.getResource());
 			return redisDataStore;
 	}
 }
