e94b4deab2e8965418d741bd32838533ad37e6ae
cecile <cecile@nulab-inc.com>
2017-10-11 09:08:52 -0400

    like the doc for redis

--- src/main/java/com/mogaleaf/Main.java
+++ src/main/java/com/mogaleaf/Main.java
@@ -5,8 +5,12 @@ import com.mogaleaf.oauth.RedisDatastoreFactory;
 import com.mogaleaf.server.Configuration;
 import com.mogaleaf.server.OAuthHttpServer;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
 
 import java.io.File;
+import java.net.URI;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,8 +21,13 @@ public class Main {
         Logger.getLogger("APPBLOG").info("STARTING");
         try {
            // Jedis jedis = new Jedis(Configuration.REDIS_HOST,Integer.valueOf(Configuration.REDIS_PORT));
-            Jedis jedis = new Jedis(Configuration.REDIS_HOST);
-            RedisDatastoreFactory redisDatastoreFactory = new RedisDatastoreFactory(jedis);
+            URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+            JedisPool pool = new JedisPool(new JedisPoolConfig(),
+                    redisURI.getHost(),
+                    redisURI.getPort(),
+                    Protocol.DEFAULT_TIMEOUT,
+                    redisURI.getUserInfo().split(":",2)[1]);
+            RedisDatastoreFactory redisDatastoreFactory = new RedisDatastoreFactory(pool.getResource());
             Configuration.DATA_STORE_FACTORY = redisDatastoreFactory;
             new OAuthHttpServer().start();
 
